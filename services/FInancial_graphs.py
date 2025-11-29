6350d6f0ee3ee563cd4aa43d4e3f1ace32274054
DoniKnows <mexhuanidoni@gmail.com>
2025-11-29 09:29:53 -0500

    New graph code and change to the api ticker

---
+++ services/FInancial_graphs.py
@@ -0,0 +1,26 @@
+import yfinance as yf
+import pandas as pd 
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import datetime as dt
+
+ticker = input("Enter stock ticker symbol: ").upper()
+stock = ticker
+enddate = dt.datetime.now()
+startdate = enddate - dt.timedelta(days=365*2)
+
+df = yf.download(stock, start=startdate, end=enddate)
+
+df.head()
+
+Close_prices= df['Close']
+
+log_returns = np.log(Close_prices / Close_prices.shift(1))
+log_returns = log_returns.dropna()
+
+Cummulative_log_returns = log_returns.cumsum()
+Cummulative_log_returns.head()
+
+Cummulative_log_returns.plot(title=f'Cummulative Log Returns for {ticker}', figsize=(10,6))
+plt.show()
